dd5ebaea1dba871e68ca007e0ec73ed319c7573a
evcareeria <erik.vesa.contact@gmail.com>
2024-10-29 14:06:43 +0200

    [testing]

---
+++ web/dist/wasm_testing_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run(): void;
+export function greet(): void;
+export function __wbindgen_start(): void;
